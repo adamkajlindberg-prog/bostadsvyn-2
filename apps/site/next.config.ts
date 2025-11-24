f0478111c3fbb957d5ba3a4a46df898b6463413f
cjkihl <carljohan.kihl@gmail.com>
2025-11-24 08:31:35 +0800

    up

--- apps/site/next.config.ts
+++ apps/site/next.config.ts
@@ -2,7 +2,26 @@ import type { NextConfig } from "next";
 
 // Import to validate env variables on build
 import "./src/env";
+import { env } from "./src/env";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  distDir: "dist",
+  async rewrites() {
+    return env.NEXT_PUBLIC_POSTHOG_HOST
+      ? [
+          {
+            destination: `${env.NEXT_PUBLIC_POSTHOG_HOST}/static/:path*`,
+            source: "/api/teddy/static/:path*",
+          },
+          {
+            destination: `${env.NEXT_PUBLIC_POSTHOG_HOST}/:path*`,
+            source: "/api/teddy/:path*",
+          },
+        ]
+      : [];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 
 export default nextConfig;
